73cbac95bb1336eefe4fe729b36b3d9d598efb00
sohan-99 <sohan75632@gmail.com>
2023-11-26 01:11:15 +0600

    Employe managment Client Site Basic Setup

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='text-8xl font-bold'>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
